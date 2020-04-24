6617c7ce431f060994881de4a86af0ad83cd0f01
Ochui Princewill <ochuiprincewill411@gmail.com>
2020-04-24 08:26:26 +0100

    update service worker

--- public/pwabuilder-sw.js
+++ public/pwabuilder-sw.js
@@ -3,6 +3,11 @@
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.0.0/workbox-sw.js');
 
 const CACHE = "pwabuilder-page";
+const HTML_CACHE = "html";
+const JS_CACHE = "javascript";
+const STYLE_CACHE = "stylesheets";
+const IMAGE_CACHE = "images";
+const FONT_CACHE = "fonts";
 
 // TODO: replace the following with the correct offline fallback page i.e.: const offlineFallbackPage = "offline.html";
 const offlineFallbackPage = "offline.html";
@@ -45,3 +50,78 @@ self.addEventListener('fetch', (event) => {
         })());
     }
 });
+
+
+self.addEventListener("message", (event) => {
+    if (event.data && event.data.type === "SKIP_WAITING") {
+        self.skipWaiting();
+    }
+});
+
+workbox.routing.registerRoute(
+    ({ event }) => event.request.destination === 'document',
+    new workbox.strategies.NetworkFirst({
+        cacheName: HTML_CACHE,
+        plugins: [
+            new workbox.expiration.ExpirationPlugin({
+                maxEntries: 10,
+            }),
+        ],
+    })
+);
+
+workbox.routing.registerRoute(
+    ({ event }) => event.request.destination === 'script',
+    new workbox.strategies.StaleWhileRevalidate({
+        cacheName: JS_CACHE,
+        plugins: [
+            new workbox.expiration.ExpirationPlugin({
+                maxEntries: 15,
+            }),
+        ],
+    })
+);
+
+workbox.routing.registerRoute(
+    ({ event }) => event.request.destination === 'style',
+    new workbox.strategies.StaleWhileRevalidate({
+        cacheName: STYLE_CACHE,
+        plugins: [
+            new workbox.expiration.ExpirationPlugin({
+                maxEntries: 15,
+            }),
+        ],
+    })
+);
+
+workbox.routing.registerRoute(
+    ({ event }) => event.request.destination === 'image',
+    new workbox.strategies.StaleWhileRevalidate({
+        cacheName: IMAGE_CACHE,
+        plugins: [
+            new workbox.expiration.ExpirationPlugin({
+                maxEntries: 15,
+            }),
+        ],
+    })
+);
+
+workbox.routing.registerRoute(
+    ({ event }) => event.request.destination === 'font',
+    new workbox.strategies.StaleWhileRevalidate({
+        cacheName: FONT_CACHE,
+        plugins: [
+            new workbox.expiration.ExpirationPlugin({
+                maxEntries: 15,
+            }),
+        ],
+    })
+);
+
+
+workbox.routing.registerRoute(
+    new RegExp('/*'),
+    new workbox.strategies.CacheFirst({
+        cacheName: CACHE
+    })
+);
